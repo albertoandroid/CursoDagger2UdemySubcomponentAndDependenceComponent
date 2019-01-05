78d033da5afa5c67508dcf4670991f5ce37b0844
Alberto <info@androiddesdecero.com>
2019-01-05 11:28:33 +0100

    03

---
+++ app/src/main/java/com/androiddesdecero/cursodagger2componentandsubcomponent/di/MotorComponent.java
@@ -0,0 +1,15 @@
+package com.androiddesdecero.cursodagger2componentandsubcomponent.di;
+
+import com.androiddesdecero.cursodagger2componentandsubcomponent.ui.Main2Activity;
+
+import dagger.Component;
+
+/**
+ * Created by albertopalomarrobledo on 5/1/19.
+ */
+
+@Component(modules = MotorModule.class)
+public interface MotorComponent {
+
+    void inject(Main2Activity main2Activity);
+}
